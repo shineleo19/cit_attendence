82338e541e5a9592eb7657de520c1f04aaa1195d
shineleo19 <shineleo19@gmail.com>
2025-11-26 22:30:04 +0530

    initial commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,26 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+    signingConfigs {
+        create("release") {
+            storeFile = file("attendance_cit.jks")
+            storePassword = "shineleo@2006"
+            keyAlias = "attendancecit"
+            keyPassword = "shineleo@2006"
+        }
+    }
 
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
